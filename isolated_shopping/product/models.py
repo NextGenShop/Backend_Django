485b2f33b1fb499ceff254a2cc24dbe4dbf7797b
Brian Min <bymi15@yahoo.com>
2021-02-10 23:34:02 +0000

    Update models.py

--- isolated_shopping/product/models.py
+++ isolated_shopping/product/models.py
@@ -1,15 +1,13 @@
 from django.db import models
-from retailer.models import RetailerDB
-
 
 # Create your models here.
 class ProductDB(models.Model):
     productId = models.AutoField(primary_key=True)
-    soldBy = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
     image = models.TextField(null=True)
     price = models.FloatField()
-    stock = models.FloatField()
+    stock = models.PositiveIntegerField()
+    retailer = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
